79d047863908c487d78b984306ee730bc935a7e9
nathannenga <nathan.nannenga@gmail.com>
2017-03-04 09:47:34 -0700

    all done

--- practice.js
+++ practice.js
@@ -6,20 +6,24 @@
 
 // Rewrite the following line so that inner is scoped to the if statement.
 if ( true ) {
-	var inner = "Inner!";
+	let inner = "Inner!";
 }
 
 // declare a variable named noChange that cannot be re-assigned and give it a value of 100.
 
+ const noChange = 100;
 
 // declare a new constant named vocab with the value of [ "let", "var", "destructuring", "spread", "rest" ]
 
+const vocab = ["let","var","destructuring","spread","rest"]
 
 // change "var" to "const" at index 1 of vocab
 
+vocab[1] = "const";
 
 // add "arrow function" to the end of vocab
 
+vocab.push("arrow function");
 
 
 ///////////////////////
@@ -28,11 +32,13 @@ if ( true ) {
 
 // Declare 2 new variables, first and last, and assign them your first and last name.
 
+let first = "Nathan";
+let last = "Nannenga";
 
 
 // Declare the variable fullName and set it equal to your full name using template literals
 
-
+let fullName = `${first} ${last}`;
 
 //////////////////////
 /* OBJECT LITERALS */
@@ -43,10 +49,21 @@ if ( true ) {
 // and lovesDevMountain should be set to true.
 // learnES6 should be a method created with object method shorthand that returns "This is awesome!"
 
-
+let me = {
+	first,
+	last,
+	learnES6: ()=>"This is awesome!",
+	lovesDevMountain:true,
+}
 
 // using computed property names create a new object named flipped where the keys are your first and last name
 // and the values are "first" and "last"
+
+let flipped = {
+	[first]: "first",
+	[last]: "last"
+
+}
 
 
 //////////////////////////////
@@ -63,7 +80,7 @@ const destructuring = {
 // using assignment destructuring create two new variables, objects and arrays,
 // with the values taken from the above object
 
-
+let {objects, arrays} = destructuring;
 
 
 // do not modify
@@ -72,7 +89,7 @@ const vocabAgain = [ "let", "const", "destructuring", "spread", "rest", "arrow f
 // using assignment destructuring, create three variables notVar, alsoNotVar, and everythingElse
 // notVar should be "let", alsoNotVar should be "const", and everythingElse should contain the rest of the array.
 
-
+let [notVar, alsoNotVar, ...everythingElse] = vocabAgain;
 
 ////////////
 /* ARRAYS */
@@ -84,7 +101,7 @@ const lastFive = [ 6, 7, 8, 9, 10 ];
 
 // using the spread operator, create a new array named oneThroughTen.
 // You should use the above arrays (and 2 missing digits of your own)
-
+let oneThroughTen = [...firstThree,4,5,...lastFive];
 
 // do not modify
 const jsVersions = [ "es5", "es6", "es2017 onward" ];
@@ -99,48 +116,52 @@ function whatToUse( past, present, future ) {
 // using the spread operator, pass each element of jsVersions as an individual argument to whatToUse
 // and save the return value in a variable named currentJS.
 
-
+let currentJS = whatToUse(...jsVersions);
 
 ///////////////
 /* FUNCTIONS */
 ///////////////
 
 // modify greeter to greet Anonymous if no name is passed.
-function greeter( name ) {
+function greeter( name = "Anonymous" ) {
 	return `Hi ${ name }!`
 }
+
 
 
 // write a function named toPower that takes two arguments, a number and an exponent,
 // and returns number to the power of exponent.
 // if no exponent is passed, the exponent should be two
 
+let toPower = (num,exp = 2) => num ** exp;
+
 
 // using rest parameters, write a function named evenOdd that takes in
 // any number of parameters and returns an object with two properties - even and odd.
 // this function should check each parameter and push it to the appropriate property.
-
-
-
+var evenOdd = (...arr) => {
+    return {
+        even: arr.filter(item => item % 2 === 0),
+        odd: arr.filter(item => item % 2 !== 0)
+    }
+}
 // write a function named multiply that takes in a num parameter and an arbitrary amount of
 // additional numbers. This function should return an array of each additional number
 // multiplied by num.
-
-
-
+var multiply = (num, ...more) => {
+    return more.map(item => item*num);
+}
 // do not modify
 const bits = [ 2, 4, 8, 16, 32, 64, 128 ];
-
 // using an arrow function and the built in .map method, create a new array
 // named mooresBits. mooresBits should be the bits array doubled.
-
-
+var mooresBits = bits.map(item => item*2);
 // do not modify
 const that = {
-	bind: "returns a function definition with the specified context"
-	, call: "invokes a function with the specified context"
-	, apply: "invokes a function with the specified context, passing an array of arguments individually"
+    bind: "returns a function definition with the specified context"
+    , call: "invokes a function with the specified context"
+    , apply: "invokes a function with the specified context, passing an array of arguments individually"
 }
-
 // using an arrow function, add a property named arrow that returns the window object
 // do this without using the window keyword. HINT: The default binding of the this keyword
+that.arrow = () => this
